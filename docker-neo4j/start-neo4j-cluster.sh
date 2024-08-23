732256c355f9dfeeb89c21b512246bdea681f908
Florian Schabasser <florian.schabasser@alumni.tuwien.ac.at>
2024-08-23 20:07:52 +0200

    adjusted startup script neo4j

--- docker-neo4j/start-neo4j-cluster.sh
+++ docker-neo4j/start-neo4j-cluster.sh
@@ -1,6 +1,6 @@
 chmod 640 neo4j.conf
 sudo rm -rf conf data import logs
-mkdir -p conf/{server1,server2,server3,server4} data/{server1,server2,server3,server4} import/{server1,server2,server3,server4} logs/{server1,server2,server3,server4}
+mkdir -p conf/{server1,server2,server3} data/{server1,server2,server3} import/{server1,server2,server3} logs/{server1,server2,server3}
 
 export USER_ID="$(id -u)"
 export GROUP_ID="$(id -g)"
