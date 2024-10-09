0eac2739d6243626c0045dbe961a99b5ae86cf12
Florian Schabasser <florian.schabasser@hotmail.de>
2024-10-09 05:23:44 +0000

    (1) neo4j-cluster-settings

--- start.sh
+++ start.sh
@@ -6,16 +6,6 @@ docker-compose up -d
 cd ../docker-kafka || exit
 docker-compose up -d
 
-#cd ../docker-neo4j-cluster || exit
-#export USER_ID="$(id -u)"
-#export GROUP_ID="$(id -g)"
-#export NEO4J_DOCKER_IMAGE=neo4j:enterprise
-#export NEO4J_EDITION=docker_compose
-#export EXTENDED_CONF=yes
-#export NEO4J_ACCEPT_LICENSE_AGREEMENT=yes
-#export NEO4J_AUTH=neo4j/password
-#docker-compose up -d
-
 cd ../docker-neo4j || exit
 docker-compose up -d
 
