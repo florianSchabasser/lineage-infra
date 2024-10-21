d894de6c693ec055a254419fffe9c11e69e475ef
Florian Schabasser <florian.schabasser@alumni.tuwien.ac.at>
2024-10-21 08:30:05 +0200

    start script bugfix

--- start-1.podman.sh
+++ start-1.podman.sh
@@ -1,7 +1,7 @@
 cd ./docker-neo4j || exit
 podman-compose stop
 podman rm neo4j
-podman volume rm $(podman volume ls -qa --filter name=docker-neo4j*)
+podman volume rm $(podman volume ls -q --filter name=docker-neo4j*)
 
 cd ../docker-lineage || exit
 podman-compose stop
